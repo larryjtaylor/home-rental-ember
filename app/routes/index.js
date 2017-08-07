93edf011f66edf13d59ae5148704c697dc026416
Calla Rudolph and Larry Taylor <calla@>
2017-08-07 13:35:06 -0700

    add updateability to rentals

--- app/routes/index.js
+++ app/routes/index.js
@@ -26,7 +26,6 @@ var rentals = [{
 export default Ember.Route.extend({
   model() {
     return this.store.findAll('rental');
-    return this.store.findAll('announcement');
   },
   actions: {
     saveRental3(params) {
@@ -34,14 +33,18 @@ export default Ember.Route.extend({
       newRental.save();
       this.transitionTo('index');
     },
+    update(rental, params) {
+      Object.keys(params).forEach(function(key) {
+        if(params[key]!==undefined) {
+          rental.set(key,params[key]);
+        }
+      });
+      rental.save();
+      this.transitionTo('index');
+    },
     destroyRental(rental) {
       rental.destroyRecord();
       this.transitionTo('index');
     },
-    // saveAnnouncement(params) {
-    //   var newAnnouncement = this.store.createRecord('announcement', params);
-    //   newAnnouncement.save();
-    //   this.transitionTo('announcement');
-    // }
   }
 });
